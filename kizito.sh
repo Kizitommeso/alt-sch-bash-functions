6f1efd31a1a76f4bc2e31b462f3c23cc280458e9
Kizitommeso <kiz.nmeso@gmail.com>
2025-01-05 14:03:45 +0100

    commit assignment kizito.sh

---
+++ kizito.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+who_am_i () {
+    echo "Hello! My name is Kizito Maduka."
+}
+
+
+my_bio() { 
+    echo -e "\nBIO \n1. I am currently a student of Altschool in the cloud engineering track. \n2. Currently pursuing a Bachelor's degree in Sociology and Anthropology at the University of Benin. \n3. I live in lagos. \n4. Enjoy listening to music and playing football in spare time."
+}
+
+
+my_goals () {
+    echo -e "\nMY GOALS \n1. To successfully finish my Altschool Cloud Engineering Program. \n2. Understanding cloud computing concepts.
+\n3. Build hands-on experience with cloud platform, and tools \n4. Mastering cloud infrastructure management."
+}
+
+
+who_am_i
+
+my_bio
+
+my_goals
